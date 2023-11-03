3dc306c18a422e550e2fca1c286850278bfbea15
Marcin Chrost <mchrost82@gmail.com>
2023-11-03 23:16:35 +0100

    Integration test for TransferService + make ApplicationRunner as conditional (not being run in tests)

--- src/main/java/eu/chrost/simplebank/SimpleBankApplicationRunner.java
+++ src/main/java/eu/chrost/simplebank/SimpleBankApplicationRunner.java
@@ -7,11 +7,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
+@ConditionalOnProperty("simple-bank.runner.active")
 @RequiredArgsConstructor
 @Slf4j
 public class SimpleBankApplicationRunner implements ApplicationRunner {
--- src/test/java/eu/chrost/simplebank/SimpleBankApplicationTests.java
+++ src/test/java/eu/chrost/simplebank/SimpleBankApplicationTests.java
@@ -1,12 +1,39 @@
 package eu.chrost.simplebank;
 
+import eu.chrost.simplebank.account.Account;
+import eu.chrost.simplebank.account.AccountRepository;
+import eu.chrost.simplebank.transfer.TransferService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SimpleBankApplicationTests {
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private TransferService transferService;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void shouldAllowToTransferMoneyBetweenTwoAccounts() {
+        var account1 = Account.builder().id(1L).balance(BigDecimal.valueOf(1000)).build();
+        var account2 = Account.builder().id(2L).balance(BigDecimal.valueOf(2000)).build();
+        accountRepository.save(account1);
+        accountRepository.save(account2);
+
+        transferService.makeTransfer(1L, 2L, BigDecimal.valueOf(200));
+
+        assertThat(accountRepository.findById(1L).get().getBalance()).isEqualTo(BigDecimal.valueOf(800));
+        assertThat(accountRepository.findById(2L).get().getBalance()).isEqualTo(BigDecimal.valueOf(2200));
     }
 }
