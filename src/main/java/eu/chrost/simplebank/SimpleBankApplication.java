789544277bc0d707a849b63f9ec475183c412a48
Marcin Chrost <mchrost82@gmail.com>
2023-11-03 23:07:02 +0100

    Simple transfer application (Spring Boot version)

--- src/main/java/eu/chrost/simplebank/SimpleBankApplication.java
+++ src/main/java/eu/chrost/simplebank/SimpleBankApplication.java
@@ -1,33 +1,12 @@
 package eu.chrost.simplebank;
 
-import eu.chrost.simplebank.account.Account;
-import eu.chrost.simplebank.account.AccountRepository;
-import eu.chrost.simplebank.transfer.TransferService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-
-@Slf4j
+@SpringBootApplication
 public class SimpleBankApplication {
 
     public static void main(String[] args) {
-        try (var applicationContext = new AnnotationConfigApplicationContext("eu.chrost.simplebank")) {
-            var accountRepository = applicationContext.getBean(AccountRepository.class);
-            var transferService = applicationContext.getBean(TransferService.class);
-
-            var account1 = Account.builder().id(1L).balance(BigDecimal.valueOf(1000)).build();
-            var account2 = Account.builder().id(2L).balance(BigDecimal.valueOf(2000)).build();
-            accountRepository.save(account1);
-            accountRepository.save(account2);
-
-            transferService.makeTransfer(1L, 2L, BigDecimal.valueOf(200));
-
-            account1 = accountRepository.findById(1L).get();
-            account2 = accountRepository.findById(2L).get();
-            log.info("a1: {}", account1.getBalance());
-            log.info("a2: {}", account2.getBalance());
-        }
+        SpringApplication.run(SimpleBankApplication.class, args);
     }
-
 }
---
+++ src/main/java/eu/chrost/simplebank/SimpleBankApplicationRunner.java
@@ -0,0 +1,36 @@
+package eu.chrost.simplebank;
+
+import eu.chrost.simplebank.account.Account;
+import eu.chrost.simplebank.account.AccountRepository;
+import eu.chrost.simplebank.transfer.TransferService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SimpleBankApplicationRunner implements ApplicationRunner {
+
+    private final AccountRepository accountRepository;
+    private final TransferService transferService;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        var account1 = Account.builder().id(1L).balance(BigDecimal.valueOf(1000)).build();
+        var account2 = Account.builder().id(2L).balance(BigDecimal.valueOf(2000)).build();
+        accountRepository.save(account1);
+        accountRepository.save(account2);
+
+        transferService.makeTransfer(1L, 2L, BigDecimal.valueOf(200));
+
+        account1 = accountRepository.findById(1L).get();
+        account2 = accountRepository.findById(2L).get();
+        log.info("a1: {}", account1.getBalance());
+        log.info("a2: {}", account2.getBalance());
+    }
+}
